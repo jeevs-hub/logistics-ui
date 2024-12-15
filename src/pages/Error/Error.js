7d7e15f7a5f142dc939771f5c78e24e9f7204291
jeevs-hub <jsg17@hotmail.co.uk>
2024-12-15 19:40:08 +0000

    Fix case sensitivity issue

--- src/pages/Error/Error.js
+++
@@ -1,8 +0,0 @@
-const ErrorPage = () => (
-  <div>
-    <h1>Error displaying page</h1>
-    <p>There was an error displaying this page.</p>
-  </div>
-);
-
-export default ErrorPage;
--- src/pages/Vehicles/Vehicles.js
+++
@@ -1,8 +0,0 @@
-const Vehicles = () => (
-    <div>
-      <h1>Vehicle Page</h1>
-      <p>This is the Vehicle page.</p>
-    </div>
-);
-
-export default Vehicles;
